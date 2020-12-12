8b7f22f684c74be1560ba6869415513a3d58b047
Margarita Korolkova <mkorolkova311@gmail.com>
2020-12-12 22:06:22 +0300

    fix branch test

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,6 +1,52 @@
 package ru.netology.domain;
 
+
+
 public class Radio {
-    int numberOfCurrentStation;
-    int soundVolume;
+    private int numberOfCurrentStation;
+    private int soundVolume;
+
+    public int getNumberOfCurrentStation() {
+        return numberOfCurrentStation;
+    }
+
+    public void setNumberOfCurrentStation(int numberOfCurrentStation) {
+        this.numberOfCurrentStation = numberOfCurrentStation;
+    }
+
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+
+    public void setSoundVolume(int soundVolume) {
+        this.soundVolume = soundVolume;
+    }
+
+    public void nextStation() {
+        if (this.numberOfCurrentStation == 9) {
+            this.numberOfCurrentStation = 0;
+        }
+        else{this.numberOfCurrentStation++;}
+
+    }
+
+    public void prevStation() {
+        if (this.numberOfCurrentStation == 0) {
+            this.numberOfCurrentStation = 9;
+        }
+        else{this.numberOfCurrentStation--;}
+
+    }
+
+    public void nextSoundVolume() {
+        if (this.soundVolume < 10) {
+            this.soundVolume++;
+        }
+    }
+
+    public void prevSoundVolume() {
+        if (this.soundVolume > 0) {
+            this.soundVolume--;
+        }
+    }
 }
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,16 +5,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTest {
     @Test
-    public void shouldCreateTest(){
+    public void shouldNextStationTest() {
         Radio Radio1 = new Radio();
-        System.out.println("Hi");
+        Radio1.setNumberOfCurrentStation(0);
+        Radio1.nextStation();
+        assertEquals(1, Radio1.getNumberOfCurrentStation());
+        Radio1.setNumberOfCurrentStation(9);
+        Radio1.nextStation();
+        assertEquals(0, Radio1.getNumberOfCurrentStation());
     }
+
     @Test
-    public void shouldCheckFieldsTest(){
+    public void shouldPrevStationTest() {
         Radio Radio1 = new Radio();
-        assertEquals(0, Radio1.numberOfCurrentStation);
-        assertEquals(0, Radio1.soundVolume);
+        Radio1.setNumberOfCurrentStation(1);
+        Radio1.prevStation();
+        assertEquals(0, Radio1.getNumberOfCurrentStation());
+        Radio1.setNumberOfCurrentStation(0);
+        Radio1.prevStation();
+        assertEquals(9, Radio1.getNumberOfCurrentStation());
     }
-}
+
+    @Test
+    public void shouldNexSoundtVolumeTest() {
+        Radio Radio1 = new Radio();
+        Radio1.setSoundVolume(9);
+        Radio1.nextSoundVolume();
+        assertEquals(10, Radio1.getSoundVolume());
+        Radio1.setSoundVolume(10);
+        Radio1.nextSoundVolume();
+        assertEquals(10, Radio1.getSoundVolume());
+    }
+
+    @Test
+    public void shouldPrevSoundVolumeTest() {
+        Radio Radio1 = new Radio();
+        Radio1.setSoundVolume(1);
+        Radio1.prevSoundVolume();
+        assertEquals(0, Radio1.getSoundVolume());
+        Radio1.setSoundVolume(0);
+        Radio1.prevSoundVolume();
+        assertEquals(0, Radio1.getSoundVolume());
+
+    }
+
+    }
 
 
